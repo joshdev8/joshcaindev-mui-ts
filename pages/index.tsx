92d72bfe10aaa1329868f9ae5b5c51698391f8fb
Josh Cain <josh.cain@coxautoinc.com>
2023-02-07 16:09:05 -0500

    adding content to main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from 'next';
 import { Box, Container, Typography } from '@mui/material';
 import { SocialIcon } from 'react-social-icons';
-import Link from '../src/components/Link';
 
 const Home: NextPage = () => {
 	return (
@@ -15,11 +14,39 @@ const Home: NextPage = () => {
 					alignItems: 'center',
 				}}
 			>
-				<Typography variant="h4" component="h1" gutterBottom>
-					Designing user-friendly experiences, one React & NextJS app at a time
+				<Typography variant="h6" component="h3" gutterBottom>
+					Welcome to my portfolio site! As a user interface engineer with a
+					passion for building modern and efficient web applications, I
+					specialize in using React and NextJS to bring ideas to life. With a
+					focus on serverless full-stack development, I have a deep
+					understanding of how to create seamless and scalable web experiences
+					that meet the needs of both users and businesses. Whether it's through
+					intuitive design, smooth navigation, or cutting-edge technology, I
+					strive to create applications that provide real value to users and
+					deliver results for businesses. Take a look around to see some of my
+					latest projects and learn more about my skills and experience.
 				</Typography>
-				<SocialIcon url="https://linkedin.com/in/joshcain8" />
-				<SocialIcon url="https://github.com/joshdev8" />
+			</Box>
+			{/* add social icons */}
+			<Box
+				sx={{
+					my: 4,
+					display: 'flex',
+					flexDirection: 'row',
+					justifyContent: 'center',
+					alignItems: 'center',
+				}}
+			>
+				<SocialIcon
+					url="https://linkedin.com/in/joshcain8"
+					style={{ height: 50, width: 50, marginRight: 10 }}
+					target="_blank"
+				/>
+				<SocialIcon
+					url="https://github.com/joshdev8"
+					style={{ height: 50, width: 50 }}
+					target="_blank"
+				/>
 			</Box>
 		</Container>
 	);
